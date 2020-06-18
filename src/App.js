dd71ef2e0fde9acacda77479ede9d00ead6bfc25
Halim Arumman <halimarumman@gmail.com>
2020-06-18 15:53:08 +0700

    fix: router

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
 import React from 'react';
 import Navbar from './components/Navbar';
-import { BrowserRouter as Router, Switch, Route, Link} from "react-router-dom";
-import Home from './Home.js';
+import { BrowserRouter as Router, Switch, Route} from "react-router-dom";
+import Home from './Home';
 
 function App() {
   return (
     <Router>
       <div className="App">
         <Navbar />
-        <Switch>
-          <Route path="/" exact components={Home} />
-        </Switch>
+        <Route exact path="/" component={Home} />
       </div>
     </Router>
   );
--- src/Home.js
+++ src/Home.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import './index.css';
-
-function Home(){
-	return(
+class Home extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			title: 'Home'
+		}
+	}
+	render() {
+		return (
 			<div className="main mr-64 mt-16 px-3 py-6 lg:-x-6 bg-blue-500 z-10">
-				<p className="text-2xl font-bold">Custom Layout</p>
+				<p className="text-2xl font-bold">{this.state.title}</p>
 			</div>
 		);
+	}
 }
 
 export default Home;
